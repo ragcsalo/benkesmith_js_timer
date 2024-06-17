92eb6692cb0c759bcbf4906ad58091c42fbc00b2
Bence Kovacs <kov.benc@gmail.com>
2024-06-17 17:32:24 +0200

    Update Timer.m
    finally it works :-)

--- src/ios/Timer.m
+++ src/ios/Timer.m
@@ -3,13 +3,21 @@
 @implementation Timer
 
 - (void)start:(CDVInvokedUrlCommand*)command {
+    NSLog(@"[Timer] Starting timer");
     [self.commandDelegate runInBackground:^{
-        self->timer = [NSTimer scheduledTimerWithTimeInterval:1.0
-                                                       target:self
-                                                     selector:@selector(fireTimer:)
-                                                     userInfo:nil
-                                                      repeats:YES];
+        NSLog(@"[Timer] Timer setup in background");
+        if (self->timer != nil) {
+            [self->timer invalidate];
+        }
+        self->timer = [NSTimer timerWithTimeInterval:1.0
+                                              target:self
+                                            selector:@selector(fireTimer:)
+                                            userInfo:nil
+                                             repeats:YES];
         [[NSRunLoop mainRunLoop] addTimer:self->timer forMode:NSRunLoopCommonModes];
+        
+        NSLog(@"[Timer] Timer scheduled on main run loop");
+
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
@@ -17,6 +25,7 @@
 
 - (void)fireTimer:(NSTimer*)timer {
     // Ensure this runs on the main thread
+    NSLog(@"[Timer] Timer fired");
     dispatch_async(dispatch_get_main_queue(), ^{
         NSString* js = @"benkesmith.plugins.timer.onTick();";
         [self.commandDelegate evalJs:js];
@@ -24,12 +33,18 @@
 }
 
 - (void)stop:(CDVInvokedUrlCommand*)command {
+    NSLog(@"[Timer] Stopping timer");
     [self.commandDelegate runInBackground:^{
-        [self->timer invalidate];
-        self->timer = nil;
+        if (self->timer != nil) {
+            [self->timer invalidate];
+            self->timer = nil;
+            NSLog(@"[Timer] Timer invalidated");
+        }
+
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
 }
 
 @end
+
