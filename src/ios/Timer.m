e6e0e1872c7feaec5738c70b1dade16888fbaf18
Bence Kovacs <kov.benc@gmail.com>
2024-06-17 16:24:43 +0200

    Update Timer.m

--- src/ios/Timer.m
+++ src/ios/Timer.m
@@ -4,26 +4,29 @@
 
 - (void)start:(CDVInvokedUrlCommand*)command {
     [self.commandDelegate runInBackground:^{
-        timer = [NSTimer scheduledTimerWithTimeInterval:1.0
-                                                 target:self
-                                               selector:@selector(fireTimer:)
-                                               userInfo:nil
-                                                repeats:YES];
-        [[NSRunLoop mainRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+        self->timer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                                       target:self
+                                                     selector:@selector(fireTimer:)
+                                                     userInfo:nil
+                                                      repeats:YES];
+        [[NSRunLoop mainRunLoop] addTimer:self->timer forMode:NSRunLoopCommonModes];
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
 }
 
 - (void)fireTimer:(NSTimer*)timer {
-    NSString* js = @"benkesmith.plugins.timer.onTick();";
-    [self.commandDelegate evalJs:js];
+    // Ensure this runs on the main thread
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSString* js = @"benkesmith.plugins.timer.onTick();";
+        [self.commandDelegate evalJs:js];
+    });
 }
 
 - (void)stop:(CDVInvokedUrlCommand*)command {
     [self.commandDelegate runInBackground:^{
-        [timer invalidate];
-        timer = nil;
+        [self->timer invalidate];
+        self->timer = nil;
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
