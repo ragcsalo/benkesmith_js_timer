b672192d8c047e77891a4ea83a997bdbedde2692
Bence Kovacs <kov.benc@gmail.com>
2024-06-18 11:31:24 +0200

    Update Timer.java

--- src/android/Timer.java
+++ src/android/Timer.java
@@ -7,15 +7,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.os.Handler;
+import android.util.Log;
 
 public class Timer extends CordovaPlugin {
     private Handler handler = new Handler();
-    private Runnable runnable;
+    private Runnable tickerRunnable;
+    private Runnable stopRunnable;
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("start")) {
-            start(callbackContext);
+            double interval = args.optDouble(0, 1.0); // Default to 1.0 second if not provided
+            double maxRuntime = args.optDouble(1, 0) * 60; // Convert minutes to seconds, 0 means no limit
+            start(interval, maxRuntime, callbackContext);
             return true;
         } else if (action.equals("stop")) {
             stop(callbackContext);
@@ -24,25 +28,54 @@ public class Timer extends CordovaPlugin {
         return false;
     }
 
-    private void start(final CallbackContext callbackContext) {
-        runnable = new Runnable() {
+    private void start(final double interval, final double maxRuntime, final CallbackContext callbackContext) {
+        stopRunnable = new Runnable() {
+            @Override
+            public void run() {
+                if (tickerRunnable != null) {
+                    handler.removeCallbacks(tickerRunnable);
+                    tickerRunnable = null;
+                }
+                callbackContext.success();
+                Log.d("Timer", "Max runtime reached, timer stopped.");
+            }
+        };
+
+        tickerRunnable = new Runnable() {
             @Override
             public void run() {
                 if (cordova.getActivity().isFinishing()) return;
-                String js = "benkesmith.plugins.timer.onTick();";
-                webView.getEngine().evaluateJavascript(js, null);
-                handler.postDelayed(this, 1000);
+                cordova.getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String js = "benkesmith.plugins.timer.onTick();";
+                        webView.getEngine().evaluateJavascript(js, null);
+                    }
+                });
+                handler.postDelayed(this, (long) (interval * 1000));
             }
         };
-        handler.postDelayed(runnable, 1000);
+
+        handler.post(tickerRunnable);
+
+        if (maxRuntime > 0) {
+            handler.postDelayed(stopRunnable, (long) (maxRuntime * 1000));
+        }
+
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
+        Log.d("Timer", "Timer started with interval: " + interval + " seconds and max runtime: " + maxRuntime + " seconds.");
     }
 
     private void stop(CallbackContext callbackContext) {
-        if (runnable != null) {
-            handler.removeCallbacks(runnable);
-            runnable = null;
+        if (tickerRunnable != null) {
+            handler.removeCallbacks(tickerRunnable);
+            tickerRunnable = null;
+        }
+        if (stopRunnable != null) {
+            handler.removeCallbacks(stopRunnable);
+            stopRunnable = null;
         }
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
+        Log.d("Timer", "Timer stopped.");
     }
 }
